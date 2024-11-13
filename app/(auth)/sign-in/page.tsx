e09559ef4313d49e27b9f67fb3550bf6b3c06413
Muhammad Ahmad Owais <130239694+being-devahmad@users.noreply.github.com>
2024-11-14 01:58:01 +0500

    bugs fixes and revisions

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -4,3 +4,21 @@ import React from 'react'
 const SignIn = () => <AuthForm type='sign-in' />
 
 export default SignIn
+
+
+// import { cookies } from 'next/headers';
+// import AuthForm from '@/components/AuthForm'
+// import React from 'react'
+// import { redirect } from 'next/navigation';
+
+// export default async function SignIn() {
+//     const cookieStore = await cookies();
+//     const user = cookieStore.get('appwrite-session');
+
+//     // If user is already logged in, redirect or handle logic
+//     if (user) {
+//         redirect('/')
+//     }
+
+//     return <AuthForm type="sign-in" />;
+// }
