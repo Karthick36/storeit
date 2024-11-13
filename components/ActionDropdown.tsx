7c9d908110da05b531ad8ba06073889d11db6772
Muhammad Ahmad Owais <130239694+being-devahmad@users.noreply.github.com>
2024-11-13 17:05:54 +0500

    FileCard done

---
+++ components/ActionDropdown.tsx
@@ -0,0 +1,12 @@
+import { Models } from 'node-appwrite'
+import React from 'react'
+
+const ActionDropdown = ({ file }: Models.Document) => {
+    return (
+        <div>
+            Action DropDown....
+        </div>
+    )
+}
+
+export default ActionDropdown
--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -6,6 +6,7 @@ import { appwriteConfig } from "../appwrite/config"
 import { constructFileUrl, getFileType, parseStringify } from "../utils"
 import { InputFile } from "node-appwrite/file";
 import { ID, Models, Query } from "node-appwrite";
+import { getCurrentUser } from "./user.actions"
 
 
 const handleError = (error: unknown, message: string) => {
@@ -61,3 +62,41 @@ export const uploadFile = async ({
         handleError(error, "Failed to upload file");
     }
 };
+
+
+const createQueries = (currentUser: Models.Document) => {
+    const queries = [
+        Query.or([
+            Query.equal('owner', [currentUser.$id]),
+            Query.contains('users', [currentUser.email])]
+        )
+    ]
+
+    // TODO : Search , Sort , Limit
+
+    return queries
+}
+
+
+export const getFiles = async () => {
+    const { databases } = await createAdminClient()
+
+    try {
+        const currentUser = await getCurrentUser()
+
+        if (!currentUser) throw new Error("User not found")
+
+        const queries = createQueries(currentUser)
+
+        const files = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.filesCollectionId,
+            queries
+        )
+
+        return parseStringify(files)
+
+    } catch (error) {
+        handleError(error, "Failed to get Files")
+    }
+}
