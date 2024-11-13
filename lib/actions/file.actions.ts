148a54bb76ef4a8f9382091f160046039e951fb1
Muhammad Ahmad Owais <130239694+being-devahmad@users.noreply.github.com>
2024-11-13 22:50:17 +0500

    filter files

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -64,42 +64,63 @@ export const uploadFile = async ({
 };
 
 
-const createQueries = (currentUser: Models.Document) => {
+const createQueries = (
+    currentUser: Models.Document,
+    types: string[],
+    // searchText: string,
+    // sort: string,
+    // limit?: number,
+) => {
     const queries = [
         Query.or([
-            Query.equal('owner', [currentUser.$id]),
-            Query.contains('users', [currentUser.email])]
-        )
-    ]
+            Query.equal("owner", [currentUser.$id]),
+            Query.contains("users", [currentUser.email]),
+        ]),
+    ];
 
-    // TODO : Search , Sort , Limit
+    if (types.length > 0) queries.push(Query.equal("type", types));
+    // if (searchText) queries.push(Query.contains("name", searchText));
+    // if (limit) queries.push(Query.limit(limit));
 
-    return queries
-}
+    // if (sort) {
+    //     const [sortBy, orderBy] = sort.split("-");
+
+    //     queries.push(
+    //         orderBy === "asc" ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy),
+    //     );
+    // }
+
+    return queries;
+};
 
 
-export const getFiles = async () => {
-    const { databases } = await createAdminClient()
+export const getFiles = async ({
+    types = [],
+    // searchText = "",
+    // sort = "$createdAt-desc",
+    // limit,
+}: GetFilesProps) => {
+    const { databases } = await createAdminClient();
 
     try {
-        const currentUser = await getCurrentUser()
+        const currentUser = await getCurrentUser();
 
-        if (!currentUser) throw new Error("User not found")
+        if (!currentUser) throw new Error("User not found");
 
-        const queries = createQueries(currentUser)
+        const queries = createQueries(currentUser, types);
 
         const files = await databases.listDocuments(
             appwriteConfig.databaseId,
             appwriteConfig.filesCollectionId,
-            queries
-        )
+            queries,
+        );
 
-        return parseStringify(files)
-
+        console.log({ files });
+        return parseStringify(files);
     } catch (error) {
-        handleError(error, "Failed to get Files")
+        handleError(error, "Failed to get files");
     }
-}
+};
 
 
 export const renameFile = async ({
