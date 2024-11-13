4fc875dd9333af5b9d972fbe73f5d1dfed8ca86d
Muhammad Ahmad Owais <130239694+being-devahmad@users.noreply.github.com>
2024-11-13 19:52:28 +0500

    modal-actions commit
    modal actions like rename, download, details, share commit

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -18,6 +18,9 @@ import { constructDownloadUrl } from '@/lib/utils'
 import Link from 'next/link'
 import { Button } from './ui/button'
 import { Input } from './ui/input'
+import { renameFile } from '@/lib/actions/file.actions'
+import { usePathname } from 'next/navigation'
+import { FileDetails, ShareInput } from './ActionsModalContent'
 
 
 
@@ -30,12 +33,51 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
     const [isLoading, setIsLoading] = useState(false);
     const [emails, setEmails] = useState<string[]>([]);
 
+    const path = usePathname()
+
     const closeAllModals = () => {
         setIsModalOpen(false);
         setIsDropdownOpen(false);
         setAction(null);
         setName(file.name);
-        //   setEmails([]);
+        setEmails([]);
+    };
+
+    const handleAction = async () => {
+        if (!action) return;
+        setIsLoading(true);
+
+        let success = false;
+
+        const actions = {
+            rename: () =>
+                renameFile({
+                    fileId: file.$id,
+                    name,
+                    extension: file.extension,
+                    path
+                }),
+            // share: () => updateFileUsers({ fileId: file.$id, emails, path }),
+            // delete: () =>
+            //     deleteFile({ fileId: file.$id, bucketFileId: file.bucketFileId, path }),
+        };
+        success = await actions[action.value as keyof typeof actions]();
+        if (success) closeAllModals();
+        setIsLoading(false);
+    };
+
+
+    const handleRemoveUser = async (email: string) => {
+        const updatedEmails = emails.filter((e) => e !== email);
+
+        // const success = await updateFileUsers({
+        //     fileId: file.$id,
+        //     emails: updatedEmails,
+        //     path,
+        // });
+
+        // if (success) setEmails(updatedEmails);
+        // closeAllModals();
     };
 
     const renderDialogContent = () => {
@@ -49,20 +91,31 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
                     <DialogTitle className="text-center text-light-100">
                         {label}
                     </DialogTitle>
-                    {value === "rename" && (
-                        <Input
-                            type="text"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
+                    {
+                        value === "rename" && (
+                            <Input
+                                type="text"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                        )
+                    }
+                    {value === "details" && <FileDetails file={file} />}
+                    {value === "share" && (
+                        <ShareInput
+                            file={file}
+                            onInputChange={setEmails}
+                            onRemove={handleRemoveUser}
                         />
                     )}
 
-                    {value === "delete" && (
-                        <p className="delete-confirmation">
-                            Are you sure you want to delete{` `}
-                            <span className="delete-file-name">{file.name}</span>?
-                        </p>
-                    )}
+                    {
+                        value === "delete" && (
+                            <p className="delete-confirmation">
+                                Are you sure you want to delete{` `}
+                                <span className="delete-file-name">{file.name}</span>?
+                            </p>
+                        )}
                 </DialogHeader>
 
                 {
@@ -74,18 +127,21 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
                                 Cancel
                             </Button>
                             <Button
-                                // onClick={handleAction}
+                                onClick={handleAction}
                                 className="modal-submit-button">
-                                <p className="capitalize">{value}</p>
-                                {isLoading && (
-                                    <Image
-                                        src="/assets/icons/loader.svg"
-                                        alt="loader"
-                                        width={24}
-                                        height={24}
-                                        className="animate-spin"
-                                    />
-                                )}
+                                <p className="capitalize">
+                                    {value}
+                                </p>
+                                {
+                                    isLoading && (
+                                        <Image
+                                            src="/assets/icons/loader.svg"
+                                            alt="loader"
+                                            width={24}
+                                            height={24}
+                                            className="animate-spin"
+                                        />
+                                    )}
                             </Button>
                         </DialogFooter>
                     )
--- components/Card.tsx
+++ components/Card.tsx
@@ -16,17 +16,14 @@ const Card = ({ file }: Models.Document) => {
                     url={file.url}
                     className='!size-20'
                     imageClassName='!size-11' />
-                    
+
                 <div className="flex flex-col items-end justify-between">
                     <ActionDropdown file={file} />
-
-                    <p className="body-1">{convertFileSize(file.size)}</p>
+                    <p className="body-1">
+                        {convertFileSize(file.size)}
+                    </p>
                 </div>
             </div>
-
-
-
-
             <div className="file-card-details">
                 <p className="subtitle-2 line-clamp-1">{file.name}</p>
                 <FormattedDateTime
